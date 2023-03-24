752eaaccb56cf772801faa0685936b28886feaf4
ashwantmanikoth <ashwanta75@gmail.com>
2023-03-24 19:07:31 -0300

    TAAP - dashboard controller changes made

--- src/main/java/com/unb/taap/controller/DashboardController.java
+++ src/main/java/com/unb/taap/controller/DashboardController.java
@@ -28,60 +28,59 @@ import java.util.List;
 @Controller
 public class DashboardController {
 
-    @RequestMapping(value = {"/dashboard"}, method = RequestMethod.GET)
-    public String createLabSession(HttpSession session, Model model) {
-        String token = String.valueOf(session.getAttribute("token"));
-        TokenValidation tokenValidation = TAAPManager.getInstance().validateToken(token);
+  @RequestMapping(value = {"/dashboard"}, method = RequestMethod.GET)
+  public String createLabSession(HttpSession session, Model model) {
+    String token = String.valueOf(session.getAttribute("token"));
+    TokenValidation tokenValidation = TAAPManager.getInstance().validateToken(token);
 
-        if (tokenValidation.isValid()) {
-            List<Student> students = new ArrayList<>();
-            if (UserType.TA.equals(tokenValidation.getUserType())) {
-                StudentCollection studentCollection = TAAPManager.getInstance()
-                        .getLabSession(tokenValidation.getLabID())
-                        .getStudentCollection();
-                if (studentCollection != null) {
-                    Iterator<Student> iterator = studentCollection.createIterator();
-                    while (iterator.hasNext()) {
-                        students.add(iterator.next());
-                    }
-                }
-            } else {
-                students.add(TAAPManager.getInstance().getLabSession(tokenValidation.getLabID()).getStudent(
-                        String.valueOf(session.getAttribute("id"))));
-            }
-
-            model.addAttribute("students", students);
-
+    if (tokenValidation.isValid()) {
+      List<Student> students = new ArrayList<>();
+      if (UserType.TA.equals(tokenValidation.getUserType())) {
+        StudentCollection studentCollection = TAAPManager.getInstance()
+            .getLabSession(tokenValidation.getLabID())
+            .getStudentCollection();
+        if (studentCollection != null) {
+          Iterator<Student> iterator = studentCollection.createIterator();
+          while (iterator.hasNext()) {
+            students.add(iterator.next());
+          }
         }
-        return "dashboard";
+      } else {
+        students.add(TAAPManager.getInstance().getLabSession(tokenValidation.getLabID()).getStudent(
+            String.valueOf(session.getAttribute("id"))));
+      }
+
+      model.addAttribute("students", students);
+
     }
+    return "dashboard";
+  }
 
-    @GetMapping("/generate-grade-report")
-    public ResponseEntity<Resource> generateGradeReport(@RequestParam String format,
-                                                        HttpSession session) {
-        String content = "";
-        String token = String.valueOf(session.getAttribute("token"));
-        TokenValidation tokenValidation = TAAPManager.getInstance().validateToken(token);
+  @GetMapping("/generate-grade-report")
+  public ResponseEntity<Resource> generateGradeReport(@RequestParam String format,
+      HttpSession session) {
+    String content = "";
+    String token = String.valueOf(session.getAttribute("token"));
+    TokenValidation tokenValidation = TAAPManager.getInstance().validateToken(token);
 
-        if (tokenValidation.isValid()) {
-            GradeExportFactory gradeExportFactory;
-            switch (format) {
-                case "json":
-                    gradeExportFactory = new JSONGradeExportFactory();
-                    break;
-                default:
-                    gradeExportFactory = new CSVGradeExportFactory();
-                    break;
-            }
-            LabSession labSession = TAAPManager.getInstance().getLabSession(tokenValidation.getLabID());
-            GradeExporter gradeExporter = gradeExportFactory.createGradeExporter();
-            content = gradeExporter.export(labSession.getStudentCollection());
-        }
-        Resource resource = new ByteArrayResource(content.getBytes(StandardCharsets.UTF_8));
-        return ResponseEntity.ok()
-                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment;filename=report." + format)
-                .contentType(MediaType.APPLICATION_OCTET_STREAM)
-                .body(resource);
+    if (tokenValidation.isValid()) {
+      GradeExportFactory gradeExportFactory;
+      switch (format) {
+        case "json":
+          gradeExportFactory = new JSONGradeExportFactory();
+          break;
+        default:
+          gradeExportFactory = new CSVGradeExportFactory();
+          break;
+      }
+      LabSession labSession = TAAPManager.getInstance().getLabSession(tokenValidation.getLabID());
+      GradeExporter gradeExporter = gradeExportFactory.createGradeExporter();
+      content = gradeExporter.export(labSession.getStudentCollection());
     }
-
+    Resource resource = new ByteArrayResource(content.getBytes(StandardCharsets.UTF_8));
+    return ResponseEntity.ok()
+        .header(HttpHeaders.CONTENT_DISPOSITION, "attachment;filename=report." + format)
+        .contentType(MediaType.APPLICATION_OCTET_STREAM)
+        .body(resource);
+  }
 }
